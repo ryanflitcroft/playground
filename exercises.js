59fbfefd27d8da42e97632f1ad6744a353578ba3
BenFrancka <benfrancka@gmail.com>
2021-11-09 11:05:05 -0800

    adds exercises and solutions

---
+++ exercises.js
@@ -0,0 +1,41 @@
+//Exercise One
+//Multiply 5 with 2, and display the result with an alert.
+//This can be verified in the console of your live served site. Write your code below.
+
+
+//Exercise Two
+//Console.log the remainder when 15 is divided by nine. This can be verified in the console of your live served site. Write your code below.
+
+
+//Exercise Three
+//Below each variable, write its data type in comments.
+const numberOfApples = 4;
+
+const homeTown = 'Midland';
+
+const fruits = ['apple', 'kiwi', 'banana', 'tangelo'];
+
+const captain = {
+    firstName: 'Malcom',
+    lastName: 'Reynolds'
+};
+
+//Exercise Four
+//Consle.log the lastName property of the below object
+const bestCharacter = {
+    firstName: 'Varric',
+    lastName: 'Tethras'
+};
+
+//Exercise Five
+//Create an object called user with a name of Bailey, and age of 32, and a loggedIn key where the value is a boolean set to true.
+
+
+//Exercise Six
+//References the object you created in exercise five
+//In a template literal, using dot notation to access object values, console.log the phrase:  'It is true that Bailey is logged in.'
+
+//Exercise Seven
+//Below is an array of books.  Access 'The Name of the Wind' and set it to a variable.  Console.log this variable.
+const books = ['The Lord of the Rings', 'Lord of the Flies', 'The Name of the Wind', 'Gone with the Wind'];
+
