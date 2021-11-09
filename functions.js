8a32e572f872f1c57707f8aa400b285f84ec5a61
BenFrancka <benfrancka@gmail.com>
2021-11-09 15:13:34 -0800

    removes function bodies for purposes of exercise

--- functions.js
+++ functions.js
@@ -1,22 +1,19 @@
 // write a function that adds the newItem to the existing array. Be sure and take a look at the test and remember that pure functions always return something!
 export function insertIntoArray(someArray, newItem) {
-    someArray.push(newItem);
-    return someArray;
+    //write your function body here
 }
 
 // write a function that removes an existing item from an array. 
 export function removeFromArray(someArray, arrayItem) {
-    someArray.pop(arrayItem);
-    return someArray;  
+     //write your function body here 
 }
 
 // write a function that takes in two arrays and returns a new array that is a combination of the first two.
 export function concatArrays(array1, array2) {
-    const newArray = array1.concat(array2);
-    return newArray;
+    //write your function body here
 }
 
 // write a function that takes in an array and returns a new array that is comprised of the first two indexes of the original array
 export function sliceArray(someArray) {
-    return someArray.slice(0, 2);
+    //write your function body here
 }
